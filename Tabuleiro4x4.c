bee7c6b9ab49d1e3cacab7fd7e27741a9fc667bb
rafaeliratan-code <rafaeliratan@alunos.utfpr.edu.br>
2026-06-10 09:22:50 -0300

    Update Tabuleiro4x4.c

--- Tabuleiro4x4.c
+++ Tabuleiro4x4.c
@@ -1,1 +1,19 @@
 // criando o código pelo gdb...
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+#define TAMANHO_TABULEIRO 4
+
+char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
+int pontuacao[3] = {0, 0, 0};
+int pulou_rodada[3] = {0, 0, 0};
+
+void PreencherTabuleiro() {
+	for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+		for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+			tabuleiro[i][j] = ' ';
+		}
+	}
+}

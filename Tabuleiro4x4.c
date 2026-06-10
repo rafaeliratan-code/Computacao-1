a221ad6dbe11b8488eba06f1c6ffdf020deda6c0
rafaeliratan-code <rafaeliratan@alunos.utfpr.edu.br>
2026-06-10 09:49:11 -0300

    Update Tabuleiro4x4.c

--- Tabuleiro4x4.c
+++ Tabuleiro4x4.c
@@ -5,10 +5,12 @@
 #include <locale.h>
 
 #define TAMANHO_TABULEIRO 4
+#define JOGADOR_X 0
+#define JOGADOR_O 1
+#define COMPUTADOR 2
 
 char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 int pontuacao[3] = {0, 0, 0};
-int pulou_rodada[3] = {0, 0, 0};
 
 void PreencherTabuleiro() {
 	for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
@@ -33,4 +35,20 @@ void DesenharTabuleiro() {
 		}
 	}
 	printf("\n");
+}
+
+void ExibirPlacar() {
+    printf("\nPLACAR ATUAL: \n");
+    printf("Jogador X: %d pts | Jogador O: %d pts | Computador: %d pts\n", 
+            pontuacao[JOGADOR_X], pontuacao[JOGADOR_O], pontuacao[COMPUTADOR]);
+}
+
+int main() {
+	int linha, coluna;
+	char simbolos[3] = {'X', 'O', 'A'};
+	setlocale(LC_ALL, "PORTUGUESE");
+    
+    DesenharTabuleiro();
+	PreencherTabuleiro();
+	ExibirPlacar();
 }
